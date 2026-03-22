c9d0b4e837da4e7c466267d2b606371ed8f1bca0
Raj Ayush <188709341+therajayush@users.noreply.github.com>
2026-03-22 14:36:53 +0530

    Create Hello.py with print and input examples
    Added a Python script that demonstrates various print statements, variable types, and user input.

---
+++ Hello.py
@@ -0,0 +1,43 @@
+print("Hello RAJ AYUSH")
+print(6)
+
+print("0601")
+print("06*01")
+print(6*7)
+print("I am a \"good boy\"\nShe is a \"good girl\"") #Yeah She is 
+# #Aythor - RAj Ayush
+
+print("hey", 0,6,0,1, sep="/", end="1302\n")  #.    
+
+# sep = Seprates multiple values within a print statement
+# end = Specify the end value to be printed 
+# \n = Cahnges the line 
+
+print("RAj")
+
+
+a = 4
+b= True 
+c="Raj"
+d=1.4
+print("the type of a is", type(a))
+print("the type of b is", type(b))
+print("the Type of c is", type(c))
+print("the type of d is", type(d))
+
+
+
+# print("the type of d is", type(d))
+
+R="13"
+D="06" 
+
+print(int(R) + int(D))
+
+V=input("Whats on your mind")
+print("your mind is on", V)
+
+x=int(input('st no'))
+y=int(input('2nd no'))
+
+print(x+y)
