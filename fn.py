4414cfc088533758dc1422836c32d633b6e784c3
Raj Ayush <188709341+therajayush@users.noreply.github.com>
2026-03-30 00:41:35 +0530

    Implement basic arithmetic functions and greeting
    Define functions for basic arithmetic operations and birthday greeting.

---
+++ fn.py
@@ -0,0 +1,42 @@
+     # calculate(57,102)
+def happybirthday(Name,Age):
+    print(f"happy birthday{Name}")
+    print(f"you are {Age}years old")
+
+    happybirthday("",21)
+    pass
+
+def addd(x,y):
+    z=x+y
+    return z
+
+def subtract(x,y):
+    z=x-y
+    return z
+
+def multiply(x,y):
+    z=x*y
+    return z
+
+def divide(x,y):
+    z=x/y
+    return z
+
+print(divide(26,3))
+print(addd(26,3))
+print(subtract(26,3))
+print(multiply(26,3))
+
+def average(p,q):
+    m=(p+q)/2
+    return m
+
+print(average(17,19))
+print(average(2,4))
+print(average(4,6))
+
+
+
+
+# return = statement is used to end a function and send result back to the caller
+# A function is a block of code which performs a specific task whenever it is called
